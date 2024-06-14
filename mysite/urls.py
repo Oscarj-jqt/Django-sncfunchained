1bff63b410697752bb16638fcd42624383181cec
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-06-14 20:10:30 +0200

    Added root URL redirection to accueil

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('sncfunchained/', include("sncfunchained.urls")),
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('sncfunchained/accueil')),  # Redirige la racine vers 'sncfunchained/accueil'
 ]
